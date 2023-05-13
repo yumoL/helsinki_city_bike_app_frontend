01b4e80a34c6ff91921153e4ca0b5efbfe86732a
Yumo Luo <yumo.luo@helsinki.fi>
2023-05-13 23:58:55 +0300

    fix: fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <p>Welcome</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
