2f2d5cc07a66fe6b790a01510caddb23c1178a50
Yumo Luo <yumo.luo@helsinki.fi>
2023-05-22 15:00:34 +0300

    fix: force to use double quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,54 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-    'node': true
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
   },
-  'settings': {
+  "settings": {
     "react": {
       "version": "detect"
     }
   },
-  'plugins': [
-    'react-hooks',
-    'react'
+  "plugins": [
+    "react-hooks",
+    "react"
   ],
-  'extends': [
-    'eslint:recommended',
-    'plugin:react/recommended',
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
   ],
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
   },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    'ecmaVersion': 2018,
-    'sourceType': 'module'
+    "ecmaVersion": 2018,
+    "sourceType": "module"
   },
-  'rules': {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'indent': [
-      'error',
+  "rules": {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "indent": [
+      "error",
       2
     ],
-    'linebreak-style': [
-      'error',
-      'unix'
+    "linebreak-style": [
+      "error",
+      "unix"
     ],
-    'semi': [
-      'error',
-      'never'
+    "semi": [
+      "error",
+      "never"
+    ],
+    "quotes": [
+      "error",
+      "double",
+      {
+        "allowTemplateLiterals": true
+      }
     ],
     "eqeqeq": "error",
     //"no-trailing-spaces": "error",
