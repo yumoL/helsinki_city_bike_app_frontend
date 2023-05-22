004a76952cf300f4edbd1c510d9f732d5aa9f62c
Yumo Luo <yumo.luo@helsinki.fi>
2023-05-22 12:51:55 +0300

    feat: Station table with pagination

--- src/components/StationList.js
+++ src/components/StationList.js
@@ -1,8 +1,72 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import Paper from '@mui/material/Paper'
+import Pagination from '@mui/material/Pagination'
+
+import stationService from "../services/station"
 
 const StationList = () => {
+  const [stations, setStations] = useState([])
+  const [pageCount, setPageCount] = useState(1)
+  const pageSize = 10
+
+  useEffect(() => {
+    const fetchStationList = async () => {
+      const res = await stationService.getStationList(0)
+      setStations(res.stationList)
+      setPageCount(Math.floor(res.count / pageSize))
+    }
+    fetchStationList()
+  }, [])
+
+  const handleChangePage = async(event, newPage) => {
+    const res = await stationService.getStationList(newPage)
+    setStations(res.stationList)
+  }
+
+
   return (
-    <div>StationList</div>
+    <div style={{ margin: "20px" }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Station ID</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Address</TableCell>
+              <TableCell align="right">City</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {stations.map((station) => (
+              <TableRow
+                key={station.sid}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {station.sid}
+                </TableCell>
+                <TableCell align="right">{station.name}</TableCell>
+                <TableCell align="right">{station.address}</TableCell>
+                <TableCell align="right">{station.city}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Pagination 
+        style={{ marginTop: "15px" }} 
+        count={pageCount} 
+        color="primary" 
+        onChange={handleChangePage}
+      />
+    </div>
   )
 }
 
--- src/services/station.js
+++ src/services/station.js
@@ -4,10 +4,14 @@ const baseUrl = `${process.env.REACT_APP_BACKEND}/api/station`
 
 const uploadStationFile = async formData => {
   const res = await axios.post(`${baseUrl}/upload`, formData)
-  console.log('return', res)
   return res.data
 }
 
+const getStationList = async (pageIndex = 0) => {
+  const res = await axios.get(`${baseUrl}/all/${pageIndex}`)
+  return res.data.data
+}
 export default {
-  uploadStationFile
+  uploadStationFile,
+  getStationList
 }
